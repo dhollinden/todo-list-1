56afc1fd20bac0c0bea91c4d0f49db4f474a622a
dhollinden <dave.hollinden@ithaka.org>
2018-05-21 07:28:16 -0400

    Update server.js to return index.html

--- server.js
+++ server.js
@@ -1,9 +1,30 @@
 const {createServer} = require("http");
+const mime = require("mime");
+const {parse} = require("url");
+const {resolve} = require("path");
+let {readFile} = require("fs");
+
+
+const baseDirectory = process.cwd();
+
+
 let server = createServer((request, response) => {
-  response.writeHead(200, {"Content-Type": "text/html"});
-  response.write(`
-    <h1>Hello!</h1>
-    <p>You asked for <code>${request.url}</code></p>`);
-  response.end();
-});
-server.listen(8000);
+  let path = urlPath(request.url);
+  let type = mime.getType(path);
+  response.writeHead(200, {"Content-Type": type});
+  readFile(path, "utf8", (error, text) => {
+    if (error) throw error;
+    response.end(text); 
+  });
+}).listen(8000);
+
+
+function urlPath(url) {
+  let {pathname} = parse(url);
+  let path = resolve(decodeURIComponent(pathname).slice(1));
+  if (path != baseDirectory &&
+      !path.startsWith(baseDirectory + "/")) {
+    throw {status: 403, body: "Forbidden"};
+  }
+  return path;
+}
